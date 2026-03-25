b92356e4e5247888c4480e8a2ffee605d4bbdf55
Dharunya Arulmurugan <dharunyaarulmurugan86@gmail.com>
2026-03-25 20:16:56 +0530

    Add files via upload

---
+++ cse_project_file.py
@@ -0,0 +1,8 @@
+print("CSE Student Project")
+
+name = "Gayathri Murugavel"
+dept = "Computer Science Engineering"
+
+print("Name:", name)
+print("Department:", dept)
+print("Welcome to my GitHub project!")
